bb1e6e7fb599c73a60ae9613a0a10439122a39c7
Abraham Nunes <abrahamjnunes@gmail.com>
2019-06-12 08:51:18 -0300

    Added boilerplate

---
+++ diversity/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Diversity."""
+
+__author__ = """Abraham Nunes"""
+__email__ = 'nunes@dal.ca'
+__version__ = '0.1.0'
---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use Diversity in a project::
+
+    import diversity
