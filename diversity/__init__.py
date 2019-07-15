36192e164439e2013b1800abe69b007880ca7396
Abraham Nunes <abrahamjnunes@gmail.com>
2019-07-15 14:57:32 -0300

    Attempt to fix problem with bootstrap loading

--- diversity/__init__.py
+++ diversity/__init__.py
@@ -10,12 +10,13 @@ __version__ = '0.1.0'
 from diversity.indices import *
 from diversity.decompositions import *
 from diversity import base
-from diversity import datasets
 from diversity import bootstrap
+from diversity import datasets
+
 
 __all__ = [
     'base',
+    'bootstrap',
     'datasets',
     'decompositions',
-    'bootstrap',
     'indices']
